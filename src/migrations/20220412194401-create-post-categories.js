7928cfa09347f8d6fe401901abc178e81181a1cc
Natã Elienai <nata.elienai@gmail.com>
2022-04-12 19:32:33 -0300

    style: rename table and migration from PostsCategories to PostCategories

--- src/migrations/20220412194401-create-post-categories.js
+++ src/migrations/20220412194401-create-post-categories.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('PostsCategories', {
+    await queryInterface.createTable('PostCategories', {
       postId: {
         type: Sequelize.INTEGER,
         allowNull: false,
@@ -29,6 +29,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('PostsCategories');
+    await queryInterface.dropTable('PostCategories');
   }
 };
