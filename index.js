60b74f67147d10e7fc60390717e796eda94207d6
Natã Elienai <nata.elienai@gmail.com>
2022-04-30 22:18:15 -0300

    chore: remove unnecessary files owned by Trybe

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 const app = require('./src/app');
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
-
-// não remova esse endpoint, e para o avaliador funcionar
-app.get('/', (request, response) => {
-  response.send();
-});
